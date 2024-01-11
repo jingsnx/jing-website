071af3641a63196417553a8ee0906ec0a616242d
jingsnx <jingsngo@gmail.com>
2024-01-11 14:24:21 -0500

    roughly finished about me section

--- src/components/About.js
+++ src/components/About.js
@@ -16,8 +16,19 @@ export default function About() {
                         hi, i'm jing! i'm a computer science student at stevens institute of technology.
                     </p>
                     <p className='about-text'>
-                        i'm interested in web development and graphic design.
+                        i'm interested in pursuing a career in web development. in my free time i enjoy doing graphic design work and building things like keyboards.
                     </p>
+                    <div className='links'>
+                        <a href='https://github.com/jingsnx' target='_blank' className='link-item'>
+                            github
+                        </a>
+                        <a href='https://www.linkedin.com/in/jing-ngo/' target='blank' className='link-item'>
+                            linkedin
+                        </a>
+                        <a className='link-item'>
+                            contact
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
